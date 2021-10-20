e4e5065a28972bff383725951d98597e325dda23
ZastaJak <87972182+ZastaJak@users.noreply.github.com>
2021-10-20 20:25:21 +0200

    Add files via upload

---
+++ Analysis_2/DMRcaller scripts/CG-Prom.R
@@ -0,0 +1,19 @@
+#!/usr/bin/Rscript
+
+library(DMRcaller)
+
+epi1<-readBismark("aligned.deduplicated.CX_report.txt.CX_report.txt")
+
+bare_grange_csv<-read.csv("1k_50_frame.csv")
+
+bare_grange<-GRanges(seqnames = bare_grange_csv[,2], ranges = IRanges(start = as.numeric(bare_grange_csv[,3]), end = as.numeric(bare_grange_csv[,4]), names = bare_grange_csv[,1]), strand = bare_grange_csv[,6])
+chloroplast_data<-epi1[seqnames(epi1)=="chloroplast"]
+rate<-(1-(sum(mcols(chloroplast_data)$readsM)/sum(mcols(chloroplast_data)$readsN)))
+epi1_corrected<-epi1
+epi1_corrected$readsM<-round((epi1$readsM)-(epi1$readsN)*(1-rate))
+epi1_corrected$readsM[epi1_corrected$readsM<0]<-0
+epi1_corrected$readsN<-round(epi1$readsN*rate)
+epi1<-epi1_corrected
+out_grange_CG_P<-analyseReadsInsideRegionsForCondition(bare_grange, epi1, "CG")
+
+save(out_grange_CG_P, file = "CG-Prom.RData")
